e212807ffb13f439638afceaca387762301b076a
Ashish Kumar Gupta <86095263+Ashish473797@users.noreply.github.com>
2022-12-27 18:08:37 +0000

    updated

---
+++ 01_basics/01_varaibles.js
@@ -0,0 +1,7 @@
+const accountId = 1234              // never update entire programe
+let accountState;                   // declaration
+let accountdEmail = "ashish473797@gmail.com" // declaration and intialization
+var accountPassword = "12345678"    // not used futher because of issue block scope and functional scope
+accountCity = "Varanasi"            // not prefer
+
+console.table([accountId, accountdEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";                // treat as js code as a newer version
+
+let name = "ashish"         // string datatype
+let age  = 22               // number range 2 power 53 to 1
+let phoneNo = 1234567891999 // bigInt 
+let married = false         // boolean  true or false
+let speed;                  // undefine
+let empty = null            // null wereas as null is an object
+let value = Symbol('hello') // symbol
+let student = { }           // object
+
+console.log(typeof null)
